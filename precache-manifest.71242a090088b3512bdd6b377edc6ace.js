1c29e6bc2fa717efe86a9023072fe508445244bb
DiegoVaz <diegovaz.ti@gmail.com>
2021-06-14 15:18:42 -0300

    Updates

---
+++ precache-manifest.71242a090088b3512bdd6b377edc6ace.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c80727265cb581814e62c3f3398e0d98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ffc8132ced428223eb6",
+    "url": "/static/css/main.73c05575.chunk.css"
+  },
+  {
+    "revision": "e23c04957c38429dd1bb",
+    "url": "/static/js/2.baac8695.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.baac8695.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ffc8132ced428223eb6",
+    "url": "/static/js/main.c6a51b41.chunk.js"
+  },
+  {
+    "revision": "d9978230fb9cc793ade4",
+    "url": "/static/js/runtime-main.c61ccbe0.js"
+  },
+  {
+    "revision": "0f576fc3271e97add6b52e57ccbf80bb",
+    "url": "/static/media/src_sounds_bell-finish.0f576fc3.mp4"
+  },
+  {
+    "revision": "16adcf7d7b17b8f6613ca11d1bcd35f2",
+    "url": "/static/media/src_sounds_bell-start.16adcf7d.mp4"
+  }
+]);
